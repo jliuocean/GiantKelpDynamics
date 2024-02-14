29593b341172cdd84a404bfef723198d1b27ab7c
Jago Stong-Wright <jagoosw@protonmail.com>
2024-02-14 19:34:33 +0000

    oops

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Oceananigans, GiantKelpDynamics, Test, Statistics, JLD2, Oceananigans.Units, CUDA
 using OceanBioME: Biogeochemistry
 
-architecture = CUDA.has_cuda() ? GPU() : CPU()
+arch = CUDA.has_cuda() ? GPU() : CPU()
 
 include("kinematics.jl")
 include("utils.jl")
